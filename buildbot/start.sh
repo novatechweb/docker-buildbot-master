da9f04a84e2a44c95a147377c7db30b78ca5d459
Andrew Cooper <andrew.cooper@novatechweb.com>
2018-12-05 16:06:42 -0600

    Add buildbot role and new playbook
    Add environment variables for parameters
    
    Enable file-based secrets provider
    
    Use internal email server
    
    Use our internal email server instead of an external relay
    
    Add codebase generator function
    
    The codebase generator takes information about a change, like repository,
    branch, and revision, and creates a predictable string describing the code.
    This is used to decide upon the builder for a change and also to tie a fork
    repository back to its parent for merge requests.

--- buildbot/start.sh
+++ buildbot/start.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-mkdir -p $HOME/.ssh
-cp -R /run/secrets/ssh/* $HOME/.ssh
-chmod -R 600 $HOME/.ssh
-
-ln -v -s -f /home/buildbot/buildbot.tac /home/buildbot/master.cfg /home/buildbot/*.py /buildbot
-
-/usr/bin/buildbot --verbose upgrade-master
-exec twistd --nodaemon --logfile=- --pidfile=/tmp/twistd.pid --python=buildbot.tac
+# Run original buildbot entrypoint
+exec "/usr/src/buildbot/docker/start_buildbot.sh"
